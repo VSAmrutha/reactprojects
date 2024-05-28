16de8960c2651adff9aa77d4ca879fcc23312350
VSAmrutha <amrutha16492@gmail.com>
2024-05-28 16:28:16 +0530

    image slider complete

--- src/components/image-slider/index.js
+++ src/components/image-slider/index.js
@@ -14,7 +14,6 @@ const ImageSlider = ({url,limit=10,page=1}) => {
             if(data){
                 setImages(()=>data)
             }
-            console.log(images)
             setLoading(false)
         }catch(err){
             setLoading(false)
@@ -27,7 +26,20 @@ const ImageSlider = ({url,limit=10,page=1}) => {
             fetchImage(url)
         }
     },[url])
-    console.log(images)
+    const handleLeftClick=()=>{
+        if(currentSlide===0){
+            setCurrentSlide(images.length-1)
+        }else{
+            setCurrentSlide((state)=>state-1)
+        }
+    }
+    const handleRightClick=()=>{
+        if(currentSlide===images.length-1){
+            setCurrentSlide(0)
+        }else{
+            setCurrentSlide((state)=>state+1)
+        }
+    }
     if(loading){
         return <div>Loading....!!!</div>
     }
@@ -36,23 +48,27 @@ const ImageSlider = ({url,limit=10,page=1}) => {
     }
   return (
     <div className='container'>
-        <BsArrowLeftCircleFill className="arrow arrow-left"/>
+        <BsArrowLeftCircleFill onClick={handleLeftClick} className="arrow arrow-left"/>
         {
             images && images.length>0?
                 images.map((imageItem)=>{
+                    // using css for hiding the rest of the images is better as doing it with conditionaly render makes few ms of images load as white
                     return <img 
-                    className='current-image'
+                    className={currentSlide ==imageItem.id?'current-image':'current-image hide-current-image'}
                     key={imageItem.id} 
                     src={imageItem.download_url} 
                     alt={imageItem.author}/>
                 })
             :null
         }
-        <BsArrowRightCircleFill className='arrow arrow-right'/>
+        <BsArrowRightCircleFill onClick={handleRightClick} className='arrow arrow-right'/>
         <span className='circle-indicators'>
             {
                 images && images.length>0?
-                images.map((_,index)=><button key={index} className='current-indicator'></button>):null
+                images.map((_,index)=><button 
+                onClick={()=>setCurrentSlide(index)}
+                key={index} 
+                className={currentSlide===index?'current-indicator':'current-indicator inactive-indicator'}></button>):null
             }
         </span>
     </div>
